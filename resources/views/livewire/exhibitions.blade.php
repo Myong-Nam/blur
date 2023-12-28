cde2c205d2da11f3c6d5653288e50c7733695aca
Myong-Nam <meimikim@gmail.com>
2023-12-28 17:21:10 -0300

    remove dev comments

--- resources/views/livewire/exhibitions.blade.php
+++ resources/views/livewire/exhibitions.blade.php
@@ -43,12 +43,6 @@
                 <p>
                     <x-exhibition-tags :tagsCsv="$exhibition->tags" />
                 </p>
-                {{-- <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a> --}}
             </div>
         </div>
         
@@ -65,9 +59,6 @@
                 observer.observe(this.$el)
             }
         }" x-init="observe"></div>
-        {{-- @if ($exhibitions->hasMorePages())
-            <x-primary-button wire:click="loadMore">Load more</x-primary-button>
-        @endif --}}
       </div>
     </div>
   </div>

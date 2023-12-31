6fa7273f2fb5785013a249d2d1b34b2b34cad480
Myong-Nam <meimikim@gmail.com>
2023-12-31 16:25:55 -0300

    add more rules to exhibition form request

--- app/Http/Requests/ExhibitionFormRequest.php
+++ app/Http/Requests/ExhibitionFormRequest.php
@@ -15,8 +15,8 @@ class ExhibitionFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required',
-            'description' => 'required',
+            'title' => 'required|max:255',
+            'description' => 'required|min:10',
             'location' => 'required',
             'start_date' => 'required|date',
             'end_date' => 'nullable|after_or_equal:start_date',
@@ -26,17 +26,25 @@ class ExhibitionFormRequest extends FormRequest
         ];
     }
 
+    /**
+     * Custom error messages for validation rules.
+     *
+     * @return array<string, string>
+     */
     public function messages()
     {
         return [
             'title.required' => 'The title field is required',
+            'title.max' => 'The title may not be greater than 255 characters',
             'description.required' => 'The description field is required',
+            'description.min' => 'The description must be at least 10 characters',
             'location.required' => 'The location field is required',
             'start_date.required' => 'The start date field is required',
-            'end_date.after_or_equal:start_date' => 'The end date must not be earlier than the start date.',
+            'end_date.after_or_equal:start_date' => 'The end date must not be earlier than the start date',
             'tags.required' => 'The tags field is required',
-            'type_id.integer' => 'The category field is required.',
-            'museum' => 'The museum field is required.',
+            'type_id.required' => 'The category field is required',
+            'type_id.integer' => 'The category must be an integer',
+            'museum.required' => 'The museum field is required',
         ];
     }
 }

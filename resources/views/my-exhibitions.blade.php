ff1b8c899daae19458e1b4e6eec07833a9a48b25
Myong-Nam <meimikim@gmail.com>
2024-01-09 19:21:50 -0300

    hide overflow text

--- resources/views/my-exhibitions.blade.php
+++ resources/views/my-exhibitions.blade.php
@@ -2,7 +2,7 @@
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 max-w-xs overflow-hidden">
                 <tr>
                     <th scope="col" class="px-6 py-3">
                         Title
@@ -27,7 +27,7 @@
             <tbody>
                 @foreach ($exhibitions as $exhibition)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white max-w-xs overflow-hidden">
                         {{$exhibition->title}}
                     </th>
                     <td class="px-6 py-4">
